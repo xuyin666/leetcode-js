81783745383fd38334c662691b206b2d946870d9
Yinn <1499978024@qq.com>
2022-07-23 19:03:58 +0200

    add 62 and 1143 and change folder name

---
+++ blind75/2d-dynamocProgramming/1143-longestCommonSubsequence.js
@@ -0,0 +1,21 @@
+// 1143. Longest Common Subsequence
+// Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
+// A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+// For example, "ace" is a subsequence of "abcde".
+// A common subsequence of two strings is a subsequence that is common to both strings.
+
+
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+var longestCommonSubsequence = function(text1, text2) {
+    
+};
+let text1 = "abcde", text2 = "ace";
+let text3 = "abc", text4 = "abc";
+let text5 = "abc", text6 = "def";
+console.log(longestCommonSubsequence(text1, text2));
+console.log(longestCommonSubsequence(text3, text4));
+console.log(longestCommonSubsequence(text5, text6));
---
+++ blind75/2d-dynamocProgramming/62-uniquePaths.js
@@ -0,0 +1,19 @@
+// 62. Unique Paths
+// There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+// Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+// The test cases are generated so that the answer will be less than or equal to 2 * 109.
+
+
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+    
+};
+
+let m = 3, n = 7;
+let m1 = 3, n2 = 2;
+console.log(uniquePaths(m, n));
+console.log(uniquePaths(m1, n2));
