672dc83fe91ff3f90781960bfb16ad51b6caddbf
Yinn <1499978024@qq.com>
2022-07-23 22:19:18 +0200

    1143 longest common subsequence

--- blind75/2d-dynamocProgramming/1143-longestCommonSubsequence.js
+++ blind75/2d-dynamocProgramming/1143-longestCommonSubsequence.js
@@ -11,7 +11,27 @@
  * @return {number}
  */
 var longestCommonSubsequence = function(text1, text2) {
-    
+    let m = text1.length;
+    let n = text2.length;
+    let dp = new Array(m + 1);
+    for (let i = 0; i <= m; i++) {
+        dp[i] = new Array(n + 1);
+    }
+    for (let i = 0; i <= m; i++) {
+        dp[i][0] = 0;
+    }
+    for (let i = 0; i <= n; i++) {
+        dp[0][i] = 0;
+    }
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (text1.charAt(i - 1) === text2.charAt(j - 1))
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            else
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+    return dp[m][n];
 };
 let text1 = "abcde", text2 = "ace";
 let text3 = "abc", text4 = "abc";
