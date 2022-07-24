8bdbc844bdc4e035bb60e5de6420fbd69d99c3c9
Yinn <1499978024@qq.com>
2022-07-24 19:56:53 +0200

    add 128 longest consecutive sequence

---
+++ blind75/arrays&Hashing/128-longestConsecutiveSequence.js
@@ -0,0 +1,16 @@
+// 128. Longest Consecutive Sequence
+// Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+// You must write an algorithm that runs in O(n) time.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    
+};
+
+let nums = [100,4,200,1,3,2];
+let nums1 = [0,3,7,2,5,8,4,6,0,1];
+console.log(longestConsecutive(nums));
+console.log(longestConsecutive(nums1));
