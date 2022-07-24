8431362e38dd370e6f3e2fe79b8d8c970fbbcad4
Yinn <1499978024@qq.com>
2022-07-24 19:46:19 +0200

    add 49 group anagrams

---
+++ blind75/arrays&Hashing/49-groupAnagrams.js
@@ -0,0 +1,19 @@
+// 49. Group Anagrams
+// Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    
+};
+
+let strs = ["eat","tea","tan","ate","nat","bat"];
+let strs1 = [""];
+let strs2 = ["a"];
+
+console.log(groupAnagrams(strs));
+console.log(groupAnagrams(strs1));
+console.log(groupAnagrams(strs2));
