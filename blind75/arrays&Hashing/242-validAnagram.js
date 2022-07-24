b4ceea119931bd3126585c1f115b124107ea0504
Yinn <1499978024@qq.com>
2022-07-24 11:21:14 +0200

    add 242 valid anagram

---
+++ blind75/arrays&Hashing/242-validAnagram.js
@@ -0,0 +1,18 @@
+// 242. Valid Anagram
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+
+};
+
+let s = "anagram", t = "nagaram";
+let s1 = "rat", t1 = "car";
+console.log(isAnagram(s, t));
+console.log(isAnagram(s1, t1));
