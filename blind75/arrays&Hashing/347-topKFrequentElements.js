f943cf8d7a993321193eb7f8dbf039556f08f619
Yinn <1499978024@qq.com>
2022-07-24 20:00:21 +0200

    347 top k frequent elements

---
+++ blind75/arrays&Hashing/347-topKFrequentElements.js
@@ -0,0 +1,16 @@
+// 347. Top K Frequent Elements
+// Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    
+};
+
+let nums = [1,1,1,2,2,3], k = 2;
+let nums1 = [1], k1 = 1;
+console.log(topKFrequent(nums, k));
+console.log(topKFrequent(nums1, k1));
