693c77675e510cb1fde82b5c7ccfc18887b3b79b
Yinn <1499978024@qq.com>
2022-07-24 22:53:51 +0200

    need to change 242 valid anagram

--- blind75/arrays&Hashing/242-validAnagram.js
+++ blind75/arrays&Hashing/242-validAnagram.js
@@ -9,7 +9,43 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-
+    let hashMap = new Map();
+    let lenS = s.length;
+    let lenT = t.length;
+    for (let i = 0; i < lenS; i++) {
+        let ch = s.charAt(i);
+        if (hashMap.has(ch)) {
+            let oldValue = hashMap.get(ch);
+            hashMap.set(ch, oldValue + 1);
+        } else {
+            hashMap.set(ch, 1);
+        }
+    }
+    for (let i = 0; i < lenT; i++) {
+        let ch = t.charAt(i);
+        if(hashMap.has(ch)) {
+            let oldValue = hashMap.get(ch);
+            hashMap.set(ch, oldValue - 1);
+        } else {
+            return false;
+        }
+    }
+    if (lenS <= lenT) {
+        for (let i = 0; i < lenT; i++) {
+            let ch = t.charAt(i);
+            if(hashMap.get(ch) !== 0) {
+                return false
+            }
+        }
+    } else {
+        for (let i = 0; i < lenS; i++) {
+            let ch = s.charAt(i);
+            if(hashMap.get(ch) !== 0) {
+                return false
+            }
+        }
+    }
+    return true;
 };
 
 let s = "anagram", t = "nagaram";
