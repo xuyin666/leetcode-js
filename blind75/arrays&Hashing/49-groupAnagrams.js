6b6f7992d5fca4fd0fade278f0add4c5851eefe9
Yin <yin.xu@cabri.com>
2022-07-26 17:16:12 +0200

    49 group Anagrams

--- blind75/arrays&Hashing/49-groupAnagrams.js
+++ blind75/arrays&Hashing/49-groupAnagrams.js
@@ -7,8 +7,31 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
+    let aMap = {};
+
+    for (let str of strs) {
+        let arr = new Array(26).fill(0);
+
+        for (let i = 0; i < str.length; i++) {
+            let index = str.charCodeAt(i) - "a".charCodeAt(0);
+            arr[index]++;
+        }
+
+        let key = arr.toString();
+        if (aMap[key] === undefined) {
+            aMap[key] = []
+        }
+        aMap[key].push(str)
+    } 
     
+    return Object.values(aMap);
 };
+
+// 这道题 用一个array，array[0-25] 记录着从'a'-'z'的出现次数
+// 再把这个array转换为string
+// 这样的话 如果是anagram，key应该相同
+// 如果key相同就把value记录下来
+
 
 let strs = ["eat","tea","tan","ate","nat","bat"];
 let strs1 = [""];
