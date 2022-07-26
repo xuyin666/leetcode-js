8717330db9d3caf031b47e0ea8c2f2d5ca07da33
Yinn <1499978024@qq.com>
2022-07-26 22:37:14 +0200

    add 238 product of array except self

---
+++ blind75/arrays&Hashing/238-productOfArrayExceptSelf.js
@@ -0,0 +1,18 @@
+// 238. Product of Array Except Self
+// Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+// You must write an algorithm that runs in O(n) time and without using the division operation.
+
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    
+};
+
+let nums = [1,2,3,4];
+let nums1 = [-1,1,0,-3,3];
+console.log(productExceptSelf(nums));
+console.log(productExceptSelf(nums1));
