05b35ed18e56a6dd83b27742a007436ca5d3f519
Yinn <1499978024@qq.com>
2022-07-27 20:46:24 +0200

    1 two sum

--- blind75/arrays&Hashing/1-twoSum.js
+++ blind75/arrays&Hashing/1-twoSum.js
@@ -9,13 +9,31 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    
+    let len = nums.length;
+    let hashMap = new Map();
+    let res = []
+    for (let i = 0; i < len; i++) {
+        let sub = target - nums[i];
+        if (hashMap.has(sub)) {
+            res.push(hashMap.get(sub))
+            res.push(i);
+            return res;
+        } else {
+            hashMap.set(nums[i], i);
+        }
+    }
+    return res;
 };
+
+// 这道题思路比较简单 就是用hashmap
+// 遍历数组 
+// 如果target - nums[i]不存在 则将nums[i]作为key，i作为value存入hashmap
+// 如果target - nums[i]存在，则返回hashmap(target - nums[i])的值 以及i值，
 
 let nums = [2,7,11,15], target = 9;
 let nums1 = [3,2,4], target1 = 6;
 let nums2 = [3,3], target2 = 6;
 
-console.log(twoSum(nums));
-console.log(twoSum(nums1));
-console.log(twoSum(nums2));
+console.log(twoSum(nums, target));
+console.log(twoSum(nums1, target1));
+console.log(twoSum(nums2, target2));
