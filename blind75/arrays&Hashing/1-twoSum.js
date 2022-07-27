c64c9fb4be6e16e3aabc5437d5a66c1533ce19e0
Yin <yin.xu@cabri.com>
2022-07-27 16:58:42 +0200

    add 1 two sum

---
+++ blind75/arrays&Hashing/1-twoSum.js
@@ -0,0 +1,21 @@
+// 1. Two Sum
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    
+};
+
+let nums = [2,7,11,15], target = 9;
+let nums1 = [3,2,4], target1 = 6;
+let nums2 = [3,3], target2 = 6;
+
+console.log(twoSum(nums));
+console.log(twoSum(nums1));
+console.log(twoSum(nums2));
