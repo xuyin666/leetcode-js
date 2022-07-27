7a788877c5abcbf9a37453eace54fa2af44bb895
Yinn <1499978024@qq.com>
2022-07-27 22:18:58 +0200

    add 15 3sum

---
+++ blind75/twoPointers/15-3Sum.js
@@ -0,0 +1,18 @@
+// 15. 3Sum
+// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+// Notice that the solution set must not contain duplicate triplets.
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    
+};
+
+let nums = [-1,0,1,2,-1,-4];
+let nums1 = [0,1,1];
+let nums2 = [0,0,0];
+console.log(threeSum(nums));
+console.log(threeSum(nums1));
+console.log(threeSum(nums2));
