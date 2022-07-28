05a5643b41f35790c4047fc3a55911ce3da751f2
Yin <yin.xu@cabri.com>
2022-07-28 10:23:07 +0200

    add 125 valid palindrome

---
+++ blind75/twoPointers/125-validPalindrome.js
@@ -0,0 +1,20 @@
+// 125. Valid Palindrome
+
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+// Given a string s, return true if it is a palindrome, or false otherwise.
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    
+};
+
+let s = "A man, a plan, a canal: Panama";
+let s1 = "race a car";
+let s2 = " ";
+
+console.log(isPalindrome(s));
+console.log(isPalindrome(s1));
+console.log(isPalindrome(s2));
