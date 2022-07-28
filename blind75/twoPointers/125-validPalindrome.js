98594da8a4c5fb28a5c8b9ab7e5624ed190bdea6
Yin <yin.xu@cabri.com>
2022-07-28 18:04:25 +0200

    try isPalindrome

--- blind75/twoPointers/125-validPalindrome.js
+++ blind75/twoPointers/125-validPalindrome.js
@@ -8,7 +8,17 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    
+    let newS = s.toLowerCase();
+    let len = newS.length;
+    for (let i = 0, j = len -1; i <= j; i++, j--) {
+        while (newS.charAt(i).charCodeAt(0) < 'a'.charCodeAt(0) || newS.charAt(i).charCodeAt(0) > 'z'.charCodeAt(0))
+            i++;
+        while (newS.charAt(j).charCodeAt(0) < 'a'.charCodeAt(0) || newS.charAt(j).charCodeAt(0) > 'z'.charCodeAt(0))
+            j--;
+        if (newS.charAt(i) !== newS.charAt(j))
+            return false;
+    } 
+    return true;
 };
 
 let s = "A man, a plan, a canal: Panama";
