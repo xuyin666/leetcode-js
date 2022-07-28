40ffb2ccc72bf6fe1a92ced607122dbaba519e07
Yin <yin.xu@cabri.com>
2022-07-28 10:46:03 +0200

    128 longest consecutive sequence

--- blind75/arrays&Hashing/128-longestConsecutiveSequence.js
+++ blind75/arrays&Hashing/128-longestConsecutiveSequence.js
@@ -7,8 +7,32 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
+    let hashSet = new Set();
+
+    for (let num of nums) {
+        hashSet.add(num);
+    }
+
+    let maxLen = 0;
+    for (let num of nums) {
+        if (!hashSet.has(num - 1)) {
+            let len = 0;
+            while(hashSet.has(num + len)) 
+                len++;
+            maxLen = Math.max(len, maxLen);
+        }
+    }
     
+    return maxLen;
 };
+
+// 思路用一个set把所有出现的值存起来(set 不存重复的值)，找得每个sequence中最右边的值
+// 然后把这个数组分成几堆
+// 比如说 nums[i] 
+// 检查是否 nums[i] - 1 存在于set中
+// 不存在则往上检查 看看是否nums[i] + 1存在于set中
+// 存在则跳过 需要发现什么时候是最左边
+
 
 let nums = [100,4,200,1,3,2];
 let nums1 = [0,3,7,2,5,8,4,6,0,1];
