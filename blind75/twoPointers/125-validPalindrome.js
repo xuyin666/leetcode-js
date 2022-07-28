5391a4eb7626bc0f9ed3fcb8d23c919c8d9d7632
Yinn <1499978024@qq.com>
2022-07-28 23:00:33 +0200

    125 valid palindrome

--- blind75/twoPointers/125-validPalindrome.js
+++ blind75/twoPointers/125-validPalindrome.js
@@ -8,18 +8,33 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    let newS = s.toLowerCase();
-    let len = newS.length;
-    for (let i = 0, j = len -1; i <= j; i++, j--) {
-        while (newS.charAt(i).charCodeAt(0) < 'a'.charCodeAt(0) || newS.charAt(i).charCodeAt(0) > 'z'.charCodeAt(0))
-            i++;
-        while (newS.charAt(j).charCodeAt(0) < 'a'.charCodeAt(0) || newS.charAt(j).charCodeAt(0) > 'z'.charCodeAt(0))
-            j--;
-        if (newS.charAt(i) !== newS.charAt(j))
+    let len = s.length;
+    let l, r;
+    for (l = 0, r = len - 1; l < r; l++, r--) {
+        while (!isAlphanumeric(s.charAt(l)) && l < r)
+            l++;
+        while (!isAlphanumeric(s.charAt(r)) && l < r)
+            r--;
+        if (s.charAt(l).toLowerCase() !== s.charAt(r).toLowerCase())
             return false;
-    } 
+    }
     return true;
 };
+
+
+var isAlphanumeric = function(ch) {
+    if ((ch.charCodeAt(0) >= 'a'.charCodeAt(0) && ch.charCodeAt(0) <= 'z'.charCodeAt(0)) 
+        || (ch.charCodeAt(0) >= 'A'.charCodeAt(0) && ch.charCodeAt(0) <= 'Z'.charCodeAt(0))
+        || (ch.charCodeAt(0) >= '0'.charCodeAt(0) && ch.charCodeAt(0) <= '9'.charCodeAt(0)))
+        return true;
+    return false;
+}
+
+// 这道题的思路有两种
+// 第一种是不太会被采纳的，先取出只有数字和字母的字符
+// 再比较正序倒序是否相同
+// 第二种 写一个文件判断是否为数字或者字母
+// 是字母或者数字就跳过，i 从 0 从前往后，j从length - 1从后往前推
 
 let s = "A man, a plan, a canal: Panama";
 let s1 = "race a car";
@@ -28,3 +43,5 @@ let s2 = " ";
 console.log(isPalindrome(s));
 console.log(isPalindrome(s1));
 console.log(isPalindrome(s2));
+
+
