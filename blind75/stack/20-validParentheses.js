e35ed63a3039cfc4d588e0575ed2d439f6feddba
Yinn <1499978024@qq.com>
2022-07-29 22:45:08 +0200

    20 valid parentheses

--- blind75/stack/20-validParentheses.js
+++ blind75/stack/20-validParentheses.js
@@ -9,9 +9,32 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    let stack = [];
+    let len = s.length;
+    let leftSymbol = ['{', '[', '('];
+    for(let i = 0; i < len; i++) {
+        if (leftSymbol.includes(s.charAt(i)))
+            stack.push(s.charAt(i))
+        else {
+            let topSymbol = stack.pop();
+            // three cases that the string is not valid
+            if (s.charAt(i) === '}' && topSymbol !== '{') 
+               return false;
+            else if (s.charAt(i) === ']' && topSymbol !== '[')
+                return false;
+            else if (s.charAt(i) === ')' && topSymbol !== '(')
+                return false
+        }
+    }
+    // to avoid the case '{{{{'
+    return stack.length === 0;
 };
 
+// 思路是用一个栈结构
+// 如果遇到 "(" , "[", "{", 就压入栈中
+// 如果遇到 ")", "]", "}" 就推出一个字符串，
+// 比较这两个字符串是否相对应
+// 最后如果这个栈是空的，意味着这个是有效的
 
 let s = "()";
 let s1 = "()[]{}";
