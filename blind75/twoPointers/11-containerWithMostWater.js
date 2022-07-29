8f8b706ae479aec000bf57f292e1e84e0c45f68c
Yinn <1499978024@qq.com>
2022-07-29 23:02:52 +0200

    11 container with most water

--- blind75/twoPointers/11-containerWithMostWater.js
+++ blind75/twoPointers/11-containerWithMostWater.js
@@ -10,8 +10,26 @@
  * @return {number}
  */
 var maxArea = function(height) {
-    
+    let len = height.length;
+    let l = 0, r = len - 1;
+    let res = 0;
+    while(l < r) {
+        // 面积取决于较小的那个数
+        let area = (r - l) * Math.min(height[l], height[r]);
+        res = Math.max(res, area);
+        if (height[l] > height[r]) r--;
+        else l++;
+    }
+    return res;
 };
+
+// 这道题的思路是双指针
+// 在右边和左边分别设置一个指针i和j
+// 面积计算公式为 (i - j) * Max(nums[i], nums[j])
+// 如果 nums[i] > nums[j] 移动j
+// 为什么移动j，因为假设移动i, (i - j)变小， 剩下一边只有可能比nums[j] 或者相等
+// 所以移动j 一定会变小面积
+// 如果移动较小的那个，面积又可能变大，所以需要移动小的那个
 
 let height = [1,8,6,2,5,4,8,3,7];
 let height1 = [1,1];
