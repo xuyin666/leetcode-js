47795ca88a7ea146aa01ded994e003edf31e276a
Yinn <1499978024@qq.com>
2022-07-29 08:16:21 +0200

    add 20 valid parentheses

---
+++ blind75/stack/20-validParentheses.js
@@ -0,0 +1,21 @@
+// 20. Valid Parentheses
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+// An input string is valid if:
+// 1.Open brackets must be closed by the same type of brackets.
+// 2.Open brackets must be closed in the correct order.
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    
+};
+
+
+let s = "()";
+let s1 = "()[]{}";
+let s2 = "(]" 
+console.log(isValid(s));
+console.log(isValid(s1));
+console.log(isValid(s2));
