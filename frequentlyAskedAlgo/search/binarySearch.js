4831d1f28c9f51caa78d5e4ed1f9e9bb21c0c014
Yinn <1499978024@qq.com>
2022-07-30 22:36:56 +0200

    binary search algo

---
+++ frequentlyAskedAlgo/search/binarySearch.js
@@ -0,0 +1,22 @@
+// o(log(n)) nums sorted array
+
+let binarySearch = function (nums, target) {
+    let l = 0, r = nums.length - 1;
+    // if nums contains only one element
+    while(l <= r) {
+        let mid = Math.floor((l + r) / 2);
+        if (nums[mid] == target)
+            return mid;
+        else if(nums[mid] < target) // target too big
+            l = mid + 1;
+        else // target too small
+            r = mid - 1;
+    }
+    return -1;
+}
+
+
+let arr = [10, 20, 30, 50, 60, 80, 110, 130, 140, 170], x = 110;
+let arr1 = [10, 20, 30, 40, 60, 110, 120, 130, 170], x1 = 175;
+console.log(binarySearch(arr, x));
+console.log(binarySearch(arr1, x1));
