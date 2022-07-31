a31abb34fff7b1e5f77fbc597465b8bd81338628
Yinn <1499978024@qq.com>
2022-07-31 08:15:08 +0200

    153 find minimun in rotated sorted array

--- blind75/binarySearch/153-findMinimumInRotatedSortedArray.js
+++ blind75/binarySearch/153-findMinimumInRotatedSortedArray.js
@@ -12,8 +12,31 @@
  * @return {number}
  */
 var findMin = function(nums) {
-    
+    let l = 0, r = nums.length - 1;
+    let res = nums[0];
+    while (l <= r) {
+        let mid = Math.floor((l + r) / 2)
+        if (nums[l] <= nums[r]) {
+            res = Math.min(res, nums[l])
+            break;
+        } else {
+            res = Math.min(res, nums[mid])
+            if (nums[mid] >= nums[l]) 
+                l = mid + 1;
+            else 
+                r = mid - 1;
+        }   
+    }
+    return res;
 };
+
+// 这道题 我没有真正的理解的理解透彻
+// 大致理解
+// 首先得发现 mid 是否在一个半段上
+// 如果 nums[l] <= nums[r] 证明 从 l 到 r单调递增，可能的min值是 nums[l]
+// 如果 nums[l] > nums[l] 证明 从 l 到 r 先增后增 分开的两段递增区间
+//      如果 nums[mid] < nums[l] 证明 mid 和 l 不在一段区间内 mid = r - 1 (后半段)
+//      如果 nums[mid] >= nums[l] 证明 mid 和 l 在一段区间内 mid = l + 1 （前半段）
 
 let nums = [3,4,5,1,2];
 let nums1 = [4,5,6,7,0,1,2];
