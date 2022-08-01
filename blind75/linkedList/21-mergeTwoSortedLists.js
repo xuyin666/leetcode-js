3df9370912e93d47a3f77c9cc5980e053e0aae8d
Yinn <1499978024@qq.com>
2022-08-01 21:51:10 +0200

    21 merge two sorted lists

--- blind75/linkedList/21-mergeTwoSortedLists.js
+++ blind75/linkedList/21-mergeTwoSortedLists.js
@@ -17,8 +17,34 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-    
+    let dummy = new ListNode();
+    let tail = dummy;
+    let head1 = list1;
+    let head2 = list2;
+
+    while(head1 !== null && head2 !== null) {
+        if (head1.val < head2.val) {
+            tail.next = head1;
+            head1 = head1.next;
+        } else {
+            tail.next = head2;
+            head2 = head2.next;
+        }
+        tail = tail.next;
+    }
+
+    if (head1 === null) 
+        tail.next = head2;
+    else if (head2 === null)
+        tail.next = head1;
+    return dummy.next;
 };
+
+// 这道题思路是 首先得有一个dummyNode 就是一个空的node
+// 避免考虑那些特殊情况
+// 然后再来遍历两个list，谁大选谁
+// 最后当一个list到最后了只需要再拼接剩下的
+
 
 let list1 = [1,2,4], list2 = [1,3,4];
 let list1a = [], list2a = [];
