053004d879fc97d85377964021953a5204a1d1e7
Yin <yin.xu@cabri.com>
2022-08-01 15:51:17 +0200

    206 reverse linked list

--- blind75/linkedList/206-reverseLinkedList.js
+++ blind75/linkedList/206-reverseLinkedList.js
@@ -13,8 +13,41 @@
  * @return {ListNode}
  */
 var reverseList = function(head) {
-    
-};
+    // 递归的话
+    // 考虑几种情况，第一 head === null 第二 head.next === null
+    // 第一种情况直接返回null
+    if (head === null) return head; 
+
+    let newHead = head;
+
+    if (head.next !== null) {
+        // newHead 代表着之后的返回值
+        // reverse head之后的listNode
+        newHead = reverseList(head.next);
+        // head.next 是 当前的下个node T
+        // T.next = head 代表着倒叙
+        head.next.next = head;
+        // head.next 如果不变会成环
+        head.next = null;
+    }
+
+    // 第二种情况 直接返回head
+    return newHead;
+}
+
+
+// var reverseList = function(head) {
+//     // iterative
+//     let prev = null;
+//     let curr = head;
+//     while (curr != null) {
+//         let tmp = curr.next;
+//         curr.next = prev;
+//         prev = curr;
+//         curr = tmp;
+//     }
+//     return prev;
+// };
 
 
 let head = [1,2,3,4,5]
