b7777f4c2ed5123e40f890c6448f018f0e92e600
Yinn <1499978024@qq.com>
2022-08-02 22:38:03 +0200

    one solution

--- blind75/trees/104-maximumDepthOfBinaryTree.js
+++ blind75/trees/104-maximumDepthOfBinaryTree.js
@@ -3,20 +3,21 @@
 // A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
 
 
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
+
+// Definition for a binary tree node.
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
+
 /**
  * @param {TreeNode} root
  * @return {number}
  */
 var maxDepth = function(root) {
-    
+    if (root === null) return 0;
+    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
 };
 
 
