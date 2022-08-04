0c3a20df2370af0bad14d844ac6923e4ab382f7f
Yinn <1499978024@qq.com>
2022-08-04 17:21:16 +0200

    104 maximum depth of binary tree

--- blind75/trees/104-maximumDepthOfBinaryTree.js
+++ blind75/trees/104-maximumDepthOfBinaryTree.js
@@ -15,10 +15,62 @@ function TreeNode(val, left, right) {
  * @param {TreeNode} root
  * @return {number}
  */
+
+// DFS preorder iterative 
+// 在这种情况下需要用 stack 来模仿那个递归stack的使用情况
+// 在一个stack中存在 node和层数
+
 var maxDepth = function(root) {
+    let stack = [];
     if (root === null) return 0;
-    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    let maxLen = 1;
+    stack.push([root, maxLen])
+    while (stack.length !== 0) {
+        let topEle = stack.pop();
+        let node = topEle[0];
+        let len = topEle[1];
+        if (node !== null) {
+            maxLen = Math.max(maxLen, len);
+            stack.push([node.right, len + 1])
+            stack.push([node.left, len + 1])
+        }
+    }
+    return maxLen;
 };
+
+
+
+// BFS
+// 宽度优先得使用queue的结构
+// 一层一层的遍历
+// 先来遍历第一层 然后把他的子元素推入queue中
+// 依此类推
+// var maxDepth = function(root) {
+//     let queue = [];
+//     let maxLen = 0;
+//     if (root !== null) 
+//         queue.push(root);  
+//     while (queue.length !== 0) {
+//         let len = queue.length;
+//         for (let i = 0; i < len; i++) {
+//             let topEle = queue.shift();
+//             if (topEle.left !== null)
+//                 queue.push(topEle.left);
+//             if (topEle.right !== null)
+//                 queue.push(topEle.right);
+//         }
+//         maxLen++;
+//     }
+//     return maxLen;
+// };
+
+
+// DFS recursive root left right 
+// preorder 
+// var maxDepth = function(root) {
+//     if (root === null) return 0;
+//     return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+// };
 
 
 let root = [3,9,20,null,null,15,7];
