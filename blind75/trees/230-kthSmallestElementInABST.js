a340571799cefb958bf67cccc7d22fff062f7e64
Yinn <1499978024@qq.com>
2022-08-05 13:49:06 +0200

    230 kth smallest element in a Bst

--- blind75/trees/230-kthSmallestElementInABST.js
+++ blind75/trees/230-kthSmallestElementInABST.js
@@ -14,9 +14,42 @@
  * @param {number} k
  * @return {number}
  */
+
+
+
+// 思路是用 dfs，inorder search, (left root right)
+
+// 下面的方法是用iterative
 var kthSmallest = function(root, k) {
-    
+    let stack = [];
+    let curr = root;
+    let index = 0;
+    while (stack.length !== 0 || curr !== null) {
+        while (curr !== null) {
+            stack.push(curr);
+            curr = curr.left;
+        }
+        curr = stack.pop();
+        index++;
+        if (index === k) 
+            return curr.val;
+        curr = curr.right;
+    }
 };
+
+// 下面的方法是用递归
+// var kthSmallest = function(root, k) {
+//     let resArr = inOrderDFS(root, []);
+//     return resArr[k - 1];
+// };
+
+// let inOrderDFS = function (root, arr) {
+//     if (root === null) return;
+//     inOrderDFS(root.left, arr);
+//     arr.push(root.val);
+//     inOrderDFS(root.right, arr);
+//     return arr;
+// }
 
 let root = [3,1,4,null,2], k = 1;
 let root1 = [5,3,6,2,4,null,null,1], k1 = 3;
