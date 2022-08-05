864d57a8dd9835c8789a8dd0d1478a856e324783
Yinn <1499978024@qq.com>
2022-08-05 14:02:33 +0200

    235 lowest common ancestor of a binary search tree

--- blind75/trees/235-lowestCommonAncestorOfABinarySearchTree.js
+++ blind75/trees/235-lowestCommonAncestorOfABinarySearchTree.js
@@ -16,8 +16,22 @@
  * @param {TreeNode} q
  * @return {TreeNode}
  */
+
+// 这道题看  这个root.val的值在哪
+// 如果root的值 大于p 和 q 的值 证明root太大了 证明得找左边
+// 如果root的值 小于p 和 q 的值 证明root太小了 证明得找右边
+// 如果刚好一边一个则直接返回root
+
+
 var lowestCommonAncestor = function(root, p, q) {
-    
+    while(root !== null) {
+        if (root.val < p.val && root.val < q.val) 
+            root = root.right;
+        else if (root.val > p.val && root.val > q.val) 
+            root = root.left;
+        else 
+            return root;
+    }
 };
 
 let root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8;
