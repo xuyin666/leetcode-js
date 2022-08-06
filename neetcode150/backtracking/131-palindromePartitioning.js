04746a8fad8d16496b395d5a8b72f56595b8c39f
Yinn <1499978024@qq.com>
2022-08-06 18:25:44 +0200

    131 palindrome partitioning

--- neetcode150/backtracking/131-palindromePartitioning.js
+++ neetcode150/backtracking/131-palindromePartitioning.js
@@ -7,8 +7,38 @@
  * @return {string[][]}
  */
 var partition = function(s) {
-    
+    let res = []
+
+    let dfs = function(left, curr) {
+        if (left === s.length) {
+            res.push([...curr]);
+            return;
+        }
+        for (let mid = left + 1; mid <= s.length; mid++) {
+            let leftStr = s.substring(left, mid);
+            if (isPalidrome(leftStr)) {
+                curr.push(leftStr);
+                dfs(mid, curr);
+                curr.pop();
+            }
+        }
+    }
+
+    dfs(0, []);
+    return res;
 };
+
+let isPalidrome = function(s) {
+    let left = 0;
+    let right = s.length - 1;
+    while(left < right) {
+        if (s.charAt(left) !== s.charAt(right)) return false;
+        left++;
+        right--;
+    }
+    return true;
+}
+
 
 let s = "aab";
 let s1 = "a";
