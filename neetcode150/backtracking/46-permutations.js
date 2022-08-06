eee7a67fe289df526bc3729bd7f3c21a49c1ad8b
Yinn <1499978024@qq.com>
2022-08-06 12:12:34 +0200

    add 46 permutations

---
+++ neetcode150/backtracking/46-permutations.js
@@ -0,0 +1,19 @@
+// 46. Permutations
+
+// Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+    
+};
+
+let nums = [1,2,3];
+let nums1 = [0,1];
+let nums2 = [1];
+console.log(permute(nums));
+console.log(permute(nums1));
+console.log(permute(nums2));
+
