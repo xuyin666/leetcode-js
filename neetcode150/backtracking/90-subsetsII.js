c222561595bd1a7008775aa62857f64cf74dee6b
Yinn <1499978024@qq.com>
2022-08-06 23:01:27 +0200

    90 subset II

--- neetcode150/backtracking/90-subsetsII.js
+++ neetcode150/backtracking/90-subsetsII.js
@@ -7,7 +7,28 @@
  * @return {number[][]}
  */
 var subsetsWithDup = function(nums) {
-    
+    let res = [];
+    // we need to sort the array because we need to 
+    // check the position of the duplicated element
+    nums.sort();
+    let dfs = function(index, curr) {
+        if (index === nums.length) {
+            res.push([...curr]);
+            return;
+        }
+        // if (index > nums.length) return;
+        // take the index element
+        curr.push(nums[index]);
+        dfs(index + 1, curr);
+        curr.pop();
+        while(index + 1 < nums.length && nums[index] === nums[index + 1]) {
+            index++;
+        }
+        // we will not take the element
+        dfs(index + 1, curr);
+    }
+    dfs(0, []);
+    return res;
 };
 
 let nums = [1,2,2];
