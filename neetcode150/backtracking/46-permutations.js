c5bb91beccb049bf3a78df9b9b0731ab7e925eef
Yinn <1499978024@qq.com>
2022-08-06 12:29:19 +0200

    46 permutation

--- neetcode150/backtracking/46-permutations.js
+++ neetcode150/backtracking/46-permutations.js
@@ -7,8 +7,28 @@
  * @return {number[][]}
  */
 var permute = function(nums) {
-    
+    let res = [];
+    let dfs = function(index, nums) {
+        if (index >= nums.length) {
+            res.push([...nums]);
+        }
+        // permutate i and i ~ length - 1
+        for (let i = index; i < nums.length; i++) {
+            swap(index, i, nums);
+            dfs(index + 1, nums);
+            swap(index, i, nums);
+        }
+    }
+    dfs(0, nums);
+    return res;
 };
+
+let swap = function(i, j, nums) {
+    let tmp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = tmp;
+    return nums;
+}
 
 let nums = [1,2,3];
 let nums1 = [0,1];
