294844a5625506bbf20a32cec96c7629737b4a49
Yinn <1499978024@qq.com>
2022-08-07 08:40:08 +0200

    51 n queens

--- neetcode150/backtracking/51-n-Queens.js
+++ neetcode150/backtracking/51-n-Queens.js
@@ -9,8 +9,49 @@
  * @return {string[][]}
  */
 var solveNQueens = function(n) {
-    
+    let res = [];
+    let board = new Array(n);
+    for (let i = 0; i < n; i++) {
+        board[i] = new Array(n).fill('.');;
+    }
+    let colSet = new Set();
+    let posDia = new Set();
+    let negDia = new Set();
+    let dfs = function(row) {
+        if (row === n) {
+            let copyBoard = [...board]
+            for (let i = 0; i < n; i++) {
+                copyBoard[i] = copyBoard[i].join('');
+            }
+            res.push(copyBoard);
+            return;
+        }
+        // to put a queen in a col
+        for (let col = 0; col < n; col++) {
+            if (colSet.has(col) || posDia.has(col + row) 
+                || negDia.has(col - row))
+                continue;
+
+            // we will take this col because it is OK in row, col, posDia, negDia
+            colSet.add(col);
+            posDia.add(col + row);
+            negDia.add(col - row);
+            board[row][col] = 'Q'
+            dfs(row + 1);
+            // clean up 
+            board[row][col] = '.'
+            colSet.delete(col);
+            posDia.delete(col + row);
+            negDia.delete(col - row);
+        }
+    }
+    dfs(0);
+    return res;
 };
+
+// 写一个dfs函数， 根据row来看
+// 然后弄三个set posDia (row + col === constant), NegDig (col - row = constant)
+// backtracking
 
 let n = 4;
 let n1 = 1;
