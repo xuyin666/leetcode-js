0669f1de6fce4eab00477e97bf988839a3d45e23
Yinn <1499978024@qq.com>
2022-08-07 23:30:33 +0200

    1899 merge triplets to form target triplets

--- neetcode150/greedy/1899-mergeTripletsToFormTargetTriplet.js
+++ neetcode150/greedy/1899-mergeTripletsToFormTargetTriplet.js
@@ -13,12 +13,31 @@
  * @return {boolean}
  */
 var mergeTriplets = function(triplets, target) {
-    
+    let res = new Set();
+    for (let triplet of triplets) {
+        if (triplet[0] > target[0] || 
+            triplet[1] > target[1] || 
+            triplet[2] > target[2])
+            continue;
+        triplet.forEach((elem, index) => {
+            if (elem === target[index])
+                res.add(index);
+        })
+    }
+    return res.size === 3;
 };
+
+
+// 这道题思路是，
+// 先遍历triplets 如果triplets中的某个值 大于target的某个值 直接跳过
+// 接下来 如果triplet 中的某个值等于 target中的值
+// 则 把这个值加入到set中去
+// 最后再比较是这个set 等于 target的长度
+
 
 let triplets = [[2,5,3],[1,8,4],[1,7,5]], target = [2,7,5];
 let triplets1 = [[3,4,5],[4,5,6]], target1 = [3,2,5];
 let triplets2 = [[2,5,3],[2,3,4],[1,2,5],[5,2,3]], target2 = [5,5,5];
 console.log(mergeTriplets(triplets, target));
 console.log(mergeTriplets(triplets1, target1));
-console.log(mergeTriplets(triplets1, target1));
+console.log(mergeTriplets(triplets2, target2));
