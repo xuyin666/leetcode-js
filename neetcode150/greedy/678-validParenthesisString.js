b3c92ce5ce64149bc0798c8e271c1571439802cb
Yinn <1499978024@qq.com>
2022-08-08 00:03:17 +0200

    678 valid parenthesis string

--- neetcode150/greedy/678-validParenthesisString.js
+++ neetcode150/greedy/678-validParenthesisString.js
@@ -12,9 +12,34 @@
  * @return {boolean}
  */
 var checkValidString = function(s) {
-    
+    let leftMin = 0;
+    let leftMax = 0;
+    for (let ch of s) {
+        if (ch === '(') {
+            leftMax++;
+            leftMin++;
+        } else if (ch === ')') {
+            leftMax--;
+            leftMin--;
+        } else {
+            leftMax++;
+            leftMin--;
+        }
+        if (leftMax < 0) return false;
+        if (leftMin < 0) leftMin = 0;
+    }
+    return leftMin === 0;
 };
 
+// 这道题的思路是用两个变量 leftMax, leftMin 来记录"("的数量
+// 如果遇到 '(' 则 leftMax 和 leftMin 同时++
+// 如果遇到 ')' 则 leftMax 和 leftMin 同时--
+// 如果遇到 '*' 则 leftMax++, leftMin--, 这代表着 * 可以作为'(' (leftMin--) 
+// 或者 ')' (leftMax++)
+// 如果最后 leftMax < 0, 返回false，因为太多')'
+// 如果每次 leftMin < 0, 则需要再次赋值为0
+// leftMin 代表'('太多了， 但是可以让 '*' 变为 ''
+// 最后验证leftMin === 0
 let s = "()";
 let s1 = "(*)";
 let s2 = "(*))";
