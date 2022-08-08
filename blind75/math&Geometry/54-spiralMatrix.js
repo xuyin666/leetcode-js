755884769b82123e59b2f381f3a2196a44f445b3
Yinn <1499978024@qq.com>
2022-08-09 00:03:26 +0200

    54 spiral matrix

--- blind75/math&Geometry/54-spiralMatrix.js
+++ blind75/math&Geometry/54-spiralMatrix.js
@@ -6,7 +6,37 @@
  * @return {number[]}
  */
 var spiralOrder = function(matrix) {
-    
+    let left = 0;
+    let right = matrix[0].length - 1;
+    let top = 0;
+    let bottom = matrix.length - 1;
+    let res = [];
+    while (left <= right && top <= bottom) {
+        // we will try to iterate throught the first row
+        for (let i = left; i <= right; i++) {
+            res.push(matrix[top][i]);
+        }
+        top++;
+        // we will try to iterate throught the last column
+        for (let i = top; i <= bottom; i++) {
+            res.push(matrix[i][right]);
+        }
+        right--;
+        // if (!(left <= right && top <= bottom))
+        if (left > right || top > bottom)
+            break;
+        // we will try to iterate throught the last row
+        for (let i = right; i >= left; i--) {
+            res.push(matrix[bottom][i]);
+        }
+        bottom--;
+        // we will try to iterate throught the first column
+        for (let i = bottom; i >= top; i--) {
+            res.push(matrix[i][left]);
+        }
+        left++;
+    }
+    return res;
 };
 
 let matrix = [[1,2,3],[4,5,6],[7,8,9]];
