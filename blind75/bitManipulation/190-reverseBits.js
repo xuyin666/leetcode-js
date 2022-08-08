4a039ec31d34ef84d08aa411497b013036b6d4ff
Yinn <1499978024@qq.com>
2022-08-08 16:25:04 +0200

    190 reverse bits

--- blind75/bitManipulation/190-reverseBits.js
+++ blind75/bitManipulation/190-reverseBits.js
@@ -9,10 +9,22 @@
  * @return {number} - a positive integer
  */
 var reverseBits = function(n) {
-    
+    let res = 0;
+    for (let i = 0; i < 32; i++) {
+
+        // (n >>> i) & 1 get the last bit
+        // n >>> i means that we put the right part some 0
+        let bit = (n >>> i) & 1;
+        // bit << (31 - i) means that we left shift to make the bit
+        // appear in the correct position 
+        res = res | bit << (31 - i);
+    }
+    // the overall effect of x >>> 0 is convert x into a 32-bit unsigned integer.
+    // convert the result to an unsigned 32-bit integer
+    return res >>> 0;
 };
 
-let n = 00000010100101000001111010011100;
-let n1 = 11111111111111111111111111111101;
-console.log(reverseBits(n));
-console.log(reverseBits(n1));
+// let n = 00000010100101000001111010011100;
+// let n1 = 11111111111111111111111111111101;
+// console.log(reverseBits(n));
+// console.log(reverseBits(n1));
