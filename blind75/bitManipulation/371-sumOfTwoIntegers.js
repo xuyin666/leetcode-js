d92ad0f403f0bcc2c83e3e623c1e96c8ad2c9ec1
Yinn <1499978024@qq.com>
2022-08-08 16:39:40 +0200

    371 sum of two integers

--- blind75/bitManipulation/371-sumOfTwoIntegers.js
+++ blind75/bitManipulation/371-sumOfTwoIntegers.js
@@ -7,8 +7,24 @@
  * @return {number}
  */
 var getSum = function(a, b) {
-    
+    while (b !== 0) {
+        let tmp = (a & b) << 1;
+        a = a ^ b;
+        b = tmp;
+    }
+    return a;
 };
+
+
+// 用9 + 11来举例
+// a ^(xor) b 相当于 进行加减，但是没有加法那个carry
+// (a & b) << 1 相当于 获得那个carry
+// 两者相加 
+// 9(1001) + 11 (1011) 
+// (a ^ b)  ((a & b) << 1)
+//  00010   10010
+//  10000   00100
+//  10100   00000
 
 let a = 1, b = 2;
 let a1 = 2, b1 = 3;
