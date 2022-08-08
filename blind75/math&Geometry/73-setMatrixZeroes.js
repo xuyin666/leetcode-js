4285d3049077e235765f6de5aebf4107993c791e
Yinn <1499978024@qq.com>
2022-08-08 23:11:45 +0200

    73 set matrix zeroes

--- blind75/math&Geometry/73-setMatrixZeroes.js
+++ blind75/math&Geometry/73-setMatrixZeroes.js
@@ -7,7 +7,42 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var setZeroes = function(matrix) {
-    
+    let rowZero = false;
+    let rowLen = matrix.length;
+    let columnLen = matrix[0].length;
+    for (let row = 0; row < rowLen; row++) {
+        for (let col = 0; col < columnLen; col++) {
+            if (matrix[row][col] === 0) {
+                if (row > 0) {
+                    matrix[row][0] = 0;
+                } else {
+                    rowZero = true;
+                }
+                matrix[0][col] = 0;
+            }
+        }
+    }
+    // we will treat the column and the row 
+    // except the first line and the first column
+    for (let row = 1; row < rowLen; row++) {
+        for (let col = 1; col < columnLen; col++) {
+            if (matrix[row][0] === 0 || matrix[0][col] === 0)
+                matrix[row][col] = 0;
+        }
+    }
+    // for the first column
+    for (let row = 0; row < rowLen; row++) {
+        if (matrix[0][0] === 0) {
+            matrix[row][0] = 0;
+        }
+    }
+    // for the first row 
+    for (let col = 0; col < columnLen; col++) {
+        if (rowZero === true) {
+            matrix[0][col] = 0;
+        }
+    }
+    return matrix;
 };
 
 let matrix = [[1,1,1],[1,0,1],[1,1,1]];
