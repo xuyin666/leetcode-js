258f34194d89eb2425708c033bd0e72bf70f077a
Yinn <1499978024@qq.com>
2022-08-08 14:41:14 +0200

    191 number of 1 bits

--- blind75/bitManipulation/191-numberOf1Bits.js
+++ blind75/bitManipulation/191-numberOf1Bits.js
@@ -8,13 +8,33 @@
  * @param {number} n - a positive integer
  * @return {number}
  */
+
 var hammingWeight = function(n) {
-    
+    let res = 0;
+    while (n !== 0) {
+        n = n & (n - 1);
+        res++;
+    }
+    return res;
 };
+// var hammingWeight = function(n) {
+//     let res = 0;
+//     while (n !== 0) {
+//         if (n % 2 === 1) {
+//             res++;
+//         }
+//         n = n >>> 1;
+//     }
+//     return res;
+// };
+
+// 有两种解题办法
+// 第一种是通过 看看最后一个bit n%2 === 1，然后右移一位
+// 第二种是通过 n = n & (n - 1) 这个每次可以消除掉一个1
 
 let n = 00000000000000000000000000001011;
 let n1 = 00000000000000000000000010000000;
-let n2 = n = 11111111111111111111111111111101;
+// let n2 = 11111111111111111111111111111101;
 console.log(hammingWeight(n));
 console.log(hammingWeight(n1));
-console.log(hammingWeight(n2));
+// console.log(hammingWeight(n2)); 
