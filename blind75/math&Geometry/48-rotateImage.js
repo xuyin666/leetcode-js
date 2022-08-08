7e9d2cebc8902a1121021083fe4eb2089c3e8081
Yinn <1499978024@qq.com>
2022-08-08 23:42:29 +0200

    48 rotate image

--- blind75/math&Geometry/48-rotateImage.js
+++ blind75/math&Geometry/48-rotateImage.js
@@ -7,10 +7,32 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var rotate = function(matrix) {
-    
+    let left = 0;
+    let right = matrix.length - 1;
+    while (left < right) {
+        // iterate throught the entire row except the last element
+        for (let i = 0; i < right - left; i++) {
+            let top = left;
+            let bottom = right;
+
+            let topLeft = matrix[top][left + i];
+
+            matrix[top][left + i] = matrix[bottom - i][left];
+
+            matrix[bottom - i][left] = matrix[bottom][right - i];
+
+            matrix[bottom][right - i] = matrix[top + i][right];
+
+            matrix[top + i][right] = topLeft;
+        }
+        left++;
+        right--;
+    }
+    return matrix;
 };
 
-
+// 可以画个图 4*4 最左边为left，最右边为right
+// 最上边为top 最下边为bottom
 let matrix = [[1,2,3],[4,5,6],[7,8,9]];
 let matrix1 = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]];
 console.log(rotate(matrix));
