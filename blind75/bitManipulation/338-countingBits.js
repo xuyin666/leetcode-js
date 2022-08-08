b3ddf5c655b77e3c7957af61b439627f6b9d74c6
Yinn <1499978024@qq.com>
2022-08-08 15:12:51 +0200

    338 counting bits

--- blind75/bitManipulation/338-countingBits.js
+++ blind75/bitManipulation/338-countingBits.js
@@ -6,8 +6,28 @@
  * @return {number[]}
  */
 var countBits = function(n) {
-    
+    let res = new Array(n + 1).fill(0);
+    let sub = 1;
+    for (let i = 1; i <= n; i++) {
+        if (i == sub * 2) {
+            sub = sub * 2;
+        }
+        res[i] = 1 + res[i - sub]
+    }
+    return res;
 };
+
+// 这道题思路是 观察 bits的规律 dp[n] = 1 + dp[n - 2的最大次方]
+// 0 = "0"  dp[0]
+// 1 = "01" dp[1] = 1 + dp[1 - 1]
+// 2 = "10" dp[2] = 1 + dp[2 - 2]
+// 3 = "11" dp[3] = 1 + dp[3 - 2]
+// 4 = "100" dp[4] = 1+ dp[4 - 4]
+// 5 = "101" dp[5] = 1 + dp[5 - 4]
+// 6 = "110" dp[6] = 1 + dp[6 - 4]
+// 7 = "111" dp[7] = 1 + dp[7 - 4]
+// 8 = "1000" dp[8] = 1 + dp[8 - 8]
+
 
 let n = 2;
 let n1 = 5;
