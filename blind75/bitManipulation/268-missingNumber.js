84b57883c2b6f6d6b6e8f622600811a058d00686
Yinn <1499978024@qq.com>
2022-08-08 14:53:07 +0200

    268 missing number

--- blind75/bitManipulation/268-missingNumber.js
+++ blind75/bitManipulation/268-missingNumber.js
@@ -6,8 +6,28 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-    
+    let res = nums.length;
+    for (let i = 0; i < nums.length; i++) {
+        res = res ^ i ^ nums[i]
+    }
+    return res;
 };
+
+
+// var missingNumber = function(nums) {
+//     let res = nums.length;
+//     for (let i = 0; i < nums.length; i++) {
+//         res = res + i - nums[i]
+//     }
+//     return res;
+// };
+
+// 这道题有两个思路
+// 第一种思路是 用把0 到 nums.length累加 然后减去 nums的总和
+// 剩下的值就是 缺的那个号码
+// 第二种思路是 把0到nums.length的值 和 nums中的所有值进行异或计算
+// xor a ^ b ^ a = b;
+
 
 let nums = [3,0,1];
 let nums1 = [0,1];
