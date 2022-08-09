cffa51405350b1cf6467a7447fddc0429552164d
Yinn <1499978024@qq.com>
2022-08-09 15:59:50 +0200

    56 merge intervals

--- blind75/intervals/56-mergeIntervals.js
+++ blind75/intervals/56-mergeIntervals.js
@@ -6,7 +6,24 @@
  * @return {number[][]}
  */
 var merge = function(intervals) {
-    
+    intervals.sort((a, b) => {
+        return a[0] - b[0];
+    })
+    let left = intervals[0][0];
+    let right = intervals[0][1];
+    let res = [];
+    for (let i = 1; i < intervals.length; i++) {
+        if (right < intervals[i][0]) {
+            res.push([left, right]);
+            left = intervals[i][0];
+            right = intervals[i][1];
+        } else {
+            left = Math.min(left, intervals[i][0]);
+            right = Math.max(right, intervals[i][1]);
+        }
+    }
+    res.push([left, right]);
+    return res;
 };
 
 let intervals = [[1,3],[2,6],[8,10],[15,18]];
