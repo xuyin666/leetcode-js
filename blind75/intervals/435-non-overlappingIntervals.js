c85176493a73bc562a0c66ccb8524f97234cb363
Yinn <1499978024@qq.com>
2022-08-09 17:04:52 +0200

    435 non overlapping intervals

--- blind75/intervals/435-non-overlappingIntervals.js
+++ blind75/intervals/435-non-overlappingIntervals.js
@@ -6,7 +6,24 @@
  * @return {number}
  */
 var eraseOverlapIntervals = function(intervals) {
-    
+    // 对于interval的问题，一般都要先排序下
+    intervals.sort((a, b) => {
+        return a[0] - b[0];
+    })
+    let res = 0;
+    let preEnd = intervals[0][1];
+    for (let i = 1; i < intervals.length; i++) {
+        // it means that we have overlapping between two intervals
+        // we need to remove the interval who end laterx
+        if (preEnd > intervals[i][0]) {
+            res++;
+            preEnd = Math.min(intervals[i][1], preEnd);
+        } else {
+            // when there is not a overlapping
+            preEnd = intervals[i][1];
+        }
+    }
+    return res;
 };
 
 let intervals = [[1,2],[2,3],[3,4],[1,3]];
