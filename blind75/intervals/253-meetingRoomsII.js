0d780f0054045c93121f4a67689c3aa94b1ee59e
Yinn <1499978024@qq.com>
2022-08-09 18:51:09 +0200

    253 meeting rooms II

--- blind75/intervals/253-meetingRoomsII.js
+++ blind75/intervals/253-meetingRoomsII.js
@@ -7,8 +7,42 @@
  * @return {number}
  */
 var minMeetingRooms = function(intervals) {
-
+    let count = 0;
+    let res = 0;
+    let starts = [];
+    let ends = [];
+    for(let i = 0; i < intervals.length; i++) {
+        starts.push(intervals[i][0]);
+        ends.push(intervals[i][1]);
+    }
+    starts.sort((a, b) => {
+        return a - b;
+    });
+    ends.sort((a, b) => {
+        return a - b;
+    });
+    let start = 0;
+    let end = 0;
+    // 当starts[start] === ends[end] 是可以当作此时是不需要会议室的
+    // 用来理解这个edge case
+    while (start < starts.length) {
+        if (starts[start] < ends[end]) {
+            count++;
+            start++;
+        } else {
+            end++;
+            count--;
+        }
+        res = Math.max(res, count);
+    }
+    return res;
 };
+
+// 这道题思路是需要把interval的第一个值，和第二个值分别聚集起来 作为数组starts，ends
+// 然后排序
+// 接着循环 starts 的数组
+// 如果starts[start] < ends[end] 则证明有一个新的会议开始了
+// 不然则代表一个会议结束了
 
 let intervals = [[0,30],[5,10],[15,20]];
 let intervals1 = [[7,10],[2,4]];
