0306c085d05cda0c923fa0ae68cd1b57bc11ad2d
Yinn <1499978024@qq.com>
2022-08-09 16:46:55 +0200

    57 insert interval

--- blind75/intervals/57-insertInterval.js
+++ blind75/intervals/57-insertInterval.js
@@ -9,8 +9,32 @@
  * @return {number[][]}
  */
 var insert = function(intervals, newInterval) {
-    
+    let res = [];
+    for (let i = 0; i < intervals.length; i++) {
+        // the overlapping is finished form here
+        if (newInterval[1] < intervals[i][0]) {
+            res.push(newInterval);
+            for (let j = i; j < intervals.length; j++) {
+                res.push(intervals[j])
+            }
+            return res;
+        } else {
+            if (newInterval[0] <= intervals[i][1]) {
+                newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+                newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            } else {
+                // 如果当前数组的最大值比新interval的最小值小，就证明他们不相交
+                res.push(intervals[i])
+            }
+        }
+    }
+    // 有可能newInterval没有被push到数组里面然后就出来了
+    res.push(newInterval);
+    return res;
 };
+
+
+
 
 let intervals = [[1,3],[6,9]], newInterval = [2,5];
 let intervals1 = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval1 = [4,8];
