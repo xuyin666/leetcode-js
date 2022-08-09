c07fea3bf59bdfc53ac649e5e32fe5e5cb89a57f
Yinn <1499978024@qq.com>
2022-08-09 16:21:40 +0200

    252 meeting rooms

--- blind75/intervals/252-meetingRooms.js
+++ blind75/intervals/252-meetingRooms.js
@@ -6,7 +6,18 @@
  * @return {boolean}
  */
 var canAttendMeetings = function(intervals) {
-
+    if (intervals.length === 0) return true;
+    intervals.sort((a, b) => {
+        return a[0] - b[0];
+    })
+    let preEnd = intervals[0][1];
+    for (let i = 1; i < intervals.length; i++) {
+        if (preEnd > intervals[i][0]) 
+            return false;
+        else 
+            preEnd = intervals[i][1];
+    }
+    return true;
 };
 
 
