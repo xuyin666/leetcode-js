98cff796bedfd724f5556105ae87634687e03ce8
Yinn <1499978024@qq.com>
2022-08-10 15:55:27 +0200

    133 clone graph

--- blind75/graphs/133-cloneGraph.js
+++ blind75/graphs/133-cloneGraph.js
@@ -20,7 +20,25 @@
  * @return {Node}
  */
 var cloneGraph = function(node) {
-    
+    if(node === null) return null;
+    // in this hashmap, we will record if the node is created 
+    let hashMap = new Map();
+
+    let dfs = function(aNode) {
+        if (hashMap.has(aNode)) {
+            return hashMap.get(aNode);
+        }
+        let nodeCopy = new Node(aNode.val);
+        hashMap.set(aNode, nodeCopy)
+        let neighbors = aNode.neighbors;
+        if (neighbors === undefined) return;
+        for (let i = 0; i < neighbors.length; i++) {
+            nodeCopy.neighbors.push(dfs(neighbors[i]));
+        }
+        return nodeCopy;
+    }
+
+    return dfs(node);
 };
 
 let adjList = [[2,4],[1,3],[2,4],[1,3]];
