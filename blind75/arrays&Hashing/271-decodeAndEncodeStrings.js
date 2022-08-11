0beebd0e651da9caac164f19d9e9d9954fcd4d4b
Yinn <1499978024@qq.com>
2022-08-11 10:17:11 +0200

    271 decode and encode strings

--- blind75/arrays&Hashing/271-decodeAndEncodeStrings.js
+++ blind75/arrays&Hashing/271-decodeAndEncodeStrings.js
@@ -10,7 +10,11 @@
  * @return {string}
  */
 var encode = function(strs) {
-    
+    let res = "";
+    for (let str of strs) {
+        res = res + str.length + "#" + str;
+    }
+    return res;
 };
 
 /**
@@ -20,10 +24,32 @@ var encode = function(strs) {
  * @return {string[]}
  */
 var decode = function(s) {
-    
+    let i = 0;
+    let res = [];
+    while (i < s.length) {
+        let j = i;
+        while (s.charAt(j) !== "#") {
+            j++;
+        }
+        // length of the str beside the '#'
+        let len = parseInt(s.substring(i, j));
+        //  length of the string
+        let str = s.substring(j + 1, j + 1 + len);
+        res.push(str);
+        i = j + 1 + len;
+    }
+    return res;
 };
+
+
+
+// 这道题的思路是用 一个数字 加上 ‘#‘ 来进行编码
+// 数字代表着之后那个字符串的长度
+// 解码的时候需要先获得那个数字 然后再把之后的字符串推入到数组中
 
 /**
  * Your functions will be called as such:
  * decode(encode(strs));
  */
+// let str = ["Hello","World"];
+// console.log(decode(encode(str)));
