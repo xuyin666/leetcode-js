44c2a75048629a872a450a2d3cbcf43efaef5f2f
Yinn <1499978024@qq.com>
2022-08-11 16:24:37 +0200

    167 two sum ii input array is sorted

--- neetcode150/twoPointers/167-twoSumII-InputArrayIsSorted.js
+++ neetcode150/twoPointers/167-twoSumII-InputArrayIsSorted.js
@@ -11,7 +11,16 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-    
+    let left = 0;
+    let right = numbers.length - 1;
+    while (numbers[left] + numbers[right] !== target) {
+        if (numbers[left] + numbers[right] > target) {
+            right--;
+        } else {
+            left++;
+        }
+    }
+    return [left + 1, right + 1];
 };
 
 let numbers = [2,7,11,15], target = 9;
