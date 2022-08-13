352970389947bdfd039c48d306e9fa8984a5ae3b
Yinn <1499978024@qq.com>
2022-08-13 20:13:19 +0200

    110 balanced binary tree

--- neetcode150/trees/110-balancedBinaryTree.js
+++ neetcode150/trees/110-balancedBinaryTree.js
@@ -17,7 +17,22 @@
  * @return {boolean}
  */
 var isBalanced = function(root) {
-    
+
+    // 这个函数返回一个数组，是否为balanced 以及 最大高度
+    let dfs = function(node) {
+        if (node === null) {
+            return [true, 0];
+        }
+        let leftNode = dfs(node.left);
+        let rightNode = dfs(node.right);
+        let isValid = (Math.abs(leftNode[1] - rightNode[1]) <= 1) && 
+                        (leftNode[0]) && (rightNode[0]);
+        let maxHeight = 1 + Math.max(leftNode[1], rightNode[1]);
+        return [isValid, maxHeight];
+    }
+
+    let res = dfs(root);
+    return res[0];
 };
 
 let root = [3,9,20,null,null,15,7];
