b9f4f7ca6c8bfe9a8082202a36533d2d5aaab042
Yinn <1499978024@qq.com>
2022-08-13 17:59:26 +0200

    1448 count good nodes in binary tree

--- neetcode150/trees/1448-countGoodNodesInBinaryTree.js
+++ neetcode150/trees/1448-countGoodNodesInBinaryTree.js
@@ -3,21 +3,58 @@
 // Return the number of good nodes in the binary tree.
 
 
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
+
+//  Definition for a binary tree node.
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
 /**
  * @param {TreeNode} root
  * @return {number}
  */
 var goodNodes = function(root) {
-    
+    if (root === null)
+        return 0;
+    let res = 0;
+    let dfs = function(node, maxValue) {
+        if (node === null)
+            return;
+        if (maxValue <= node.val) 
+            res += 1;
+        maxValue = Math.max(maxValue, node.val);
+        dfs(node.left, maxValue);
+        dfs(node.right, maxValue);
+    }
+    dfs(root, root.val)
+    return res;
 };
+
+// 我的思路 
+// 用dfs 把 当前path的所有值都加入进去，
+// 在比较当前值和path里面的值
+// var goodNodes = function(root) {
+//     if (root === null)
+//         return 0;
+//     let path = [];
+//     path.push(root.val);
+//     let res = 0;
+//     let dfs = function(node) {
+//         if (node === null)
+//             return;
+//         if (Math.max(...path) <= node.val) {
+//             res += 1;
+//         }
+//         path.push(node.val);
+//         dfs(node.left);
+//         dfs(node.right);
+//         path.pop(node.val);
+//     }
+//     dfs(root)
+//     return res;
+// };
+
 
 let root = [3,1,4,3,null,1,5];
 let root1 = [3,3,null,4,2];
