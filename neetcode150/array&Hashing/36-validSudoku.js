b976b2941185c6448bceb1018b6a54a96b1bb62b
Yinn <1499978024@qq.com>
2022-08-13 21:01:56 +0200

    36 valid sudoku

--- neetcode150/array&Hashing/36-validSudoku.js
+++ neetcode150/array&Hashing/36-validSudoku.js
@@ -12,8 +12,85 @@
  * @return {boolean}
  */
 var isValidSudoku = function(board) {
-    
-};
+    let rowMap = new Map();
+    let colMap = new Map();
+    let cellMap = new Map();
+    for (let i = 0; i < 9; i++) {
+        rowMap.set(i, new Set());
+        colMap.set(i, new Set());
+    }
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            let str = i + "," + j;
+            cellMap.set(str, new Set());
+        }
+    }
+    for (let row = 0; row < 9; row++) {
+        for (let col = 0; col < 9; col++) {
+            if (board[row][col] === ".") {
+                continue;
+            }
+            let subBox = Math.floor(row / 3) + "," + Math.floor(col / 3);
+            if (rowMap.get(row).has(board[row][col]) ||
+                colMap.get(col).has(board[row][col]) ||
+                cellMap.get(subBox).has(board[row][col])) { 
+                return false;
+            }
+            rowMap.get(row).add(board[row][col]);
+            colMap.get(col).add(board[row][col]);
+            cellMap.get(subBox).add(board[row][col]);
+        }
+    }
+    return true;
+}
+
+// var isValidSudoku = function(board) {
+//     // 我的思路是 每一个cell，我都去验证在对应的set是否存在，如果存在，则证明这不是有效的数独
+//     let rowValid = function(row, column) {
+//         let rowSet = new Set();
+//         for (let i = 0; i < 9; i++) {
+//             if (i === column || board[row][i] === '.')
+//                 continue;
+//             rowSet.add(board[row][i])
+//         }
+
+//         return !rowSet.has(board[row][column]);
+//     }
+
+//     let columnValid = function(row, column) {
+//         let columnSet = new Set();
+//         for (let i = 0; i < 9; i++) {
+//             if (i === row || board[i][column] === '.')
+//                 continue;
+//             columnSet.add(board[i][column]);
+//         }
+//         return !columnSet.has(board[row][column]);
+//     }
+
+//     let subBoxValid = function(row, column) {
+//         let subBoxSet = new Set(); 
+//         let rowNb = Math.floor(row / 3);
+//         let columnNb = Math.floor(column / 3);
+//         for (let i = rowNb * 3; i < (rowNb + 1) * 3; i++) {
+//             for (let j = columnNb * 3; j < (columnNb + 1) * 3; j++) {
+//                 if ((i === row && j === column) || board[i][j] === ".") {
+//                     continue;
+//                 }
+//                 subBoxSet.add(board[i][j]);
+//             }
+//         }
+//         return !subBoxSet.has(board[row][column]);
+//     }
+
+//     for (let i = 0; i < 9; i++) {
+//         for (let j = 0; j < 9; j++) {
+//             if (!rowValid(i, j) || !columnValid(i, j) || !subBoxValid(i, j)) {
+//                 return false;
+//             }
+//         }
+//     }
+//     return true;
+// };
 
 let board = 
 [["5","3",".",".","7",".",".",".","."]
