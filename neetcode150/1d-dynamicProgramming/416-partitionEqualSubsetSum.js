01f611833a211c05f9d5bcb3dae2c2e043aac7f6
Yinn <1499978024@qq.com>
2022-08-13 22:09:43 +0200

    add 1d-dynamicProgramming

---
+++ neetcode150/1d-dynamicProgramming/416-partitionEqualSubsetSum.js
@@ -0,0 +1,15 @@
+// 416. Partition Equal Subset Sum
+// Given a non-empty array nums containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canPartition = function(nums) {
+    
+};
+
+let nums = [1,5,11,5];
+let nums1 = [1,2,3,5];
+console.log(canPartition(nums));
+console.log(canPartition(nums1));
