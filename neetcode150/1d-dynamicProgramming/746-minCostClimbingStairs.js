9675392b55799217c42c87354602bc104b4cb2d3
Yinn <1499978024@qq.com>
2022-08-14 09:34:31 +0200

    746 min cost climbing stairs

--- neetcode150/1d-dynamicProgramming/746-minCostClimbingStairs.js
+++ neetcode150/1d-dynamicProgramming/746-minCostClimbingStairs.js
@@ -7,9 +7,25 @@
  * @param {number[]} cost
  * @return {number}
  */
-var minCostClimbingStairs = function(cost) {
-    
-};
+var minCostClimbingStairs = function(cost) { 
+    // 往数组内加一个elem，为0
+    cost.push(0);
+    for (let i = cost.length - 3; i >= 0; i--) {
+        cost[i] = cost[i] + Math.min(cost[i + 1], cost[i + 2]);
+    }
+    return Math.min(cost[0], cost[1]);
+}
+
+
+// var minCostClimbingStairs = function(cost) {
+//     let len = cost.length;
+//     let dp = new Array(len).fill(cost[len - 1]);
+//     dp[len - 2] = cost[len - 2];
+//     for (let i = len - 3; i >= 0; i--) {
+//         dp[i] = cost[i] + Math.min(dp[i + 1], dp[i + 2]);
+//     }
+//     return Math.min(dp[0], dp[1]);
+// };
 
 let cost = [10,15,20];
 let cost1 = [1,100,1,1,1,100,1,1,100,1];
