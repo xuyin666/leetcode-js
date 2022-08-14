437b8d67a13d89d7f9515609a0f21c81cb1bd19d
Yinn <1499978024@qq.com>
2022-08-14 09:44:34 +0200

    416 partition equal subset sum

--- neetcode150/1d-dynamicProgramming/416-partitionEqualSubsetSum.js
+++ neetcode150/1d-dynamicProgramming/416-partitionEqualSubsetSum.js
@@ -5,9 +5,40 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+
 var canPartition = function(nums) {
-    
-};
+    // 这道题的思路就是用一个set 记录所以可能的值和
+    // 最后再看target是否存在于这个set中
+    let sum = nums.reduce((partialSum, a) => partialSum + a, 0);
+    if (sum % 2 === 1) return false;
+    let target = sum / 2;
+    let sumSet = new Set();
+    // it means that if the target is 0, that can be ok;
+    sumSet.add(0);
+    for (let i = 0; i < nums.length; i++) {
+        let aSet = new Set();
+        for (let ele of sumSet) {
+            aSet.add(ele);
+            aSet.add(ele + nums[i]);
+        }
+        sumSet = aSet;
+    }
+    return sumSet.has(target);
+}
+
+
+// var canPartition = function(nums) {
+// 用dfs 那个复杂度太高了，会有超时的问题
+//     const sum = nums.reduce((partialSum, a) => partialSum + a, 0);
+//     if (sum % 2 === 1) return false;
+//     // 当前index为i，查找的值为sum
+//     let dfs = function(sum, i) {
+//         if (sum < 0 || i >= nums.length) return false;
+//         if (sum === 0) return true;
+//         return dfs(sum, i + 1) || dfs(sum - nums[i], i + 1);
+//     }
+//     return dfs(sum / 2, 0);
+// };
 
 let nums = [1,5,11,5];
 let nums1 = [1,2,3,5];
