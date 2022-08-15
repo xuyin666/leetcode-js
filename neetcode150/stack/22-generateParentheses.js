ec34865920472b23655d1bd180a4b070db7e21be
Yinn <1499978024@qq.com>
2022-08-15 14:02:39 +0200

    22 generate parentheses

--- neetcode150/stack/22-generateParentheses.js
+++ neetcode150/stack/22-generateParentheses.js
@@ -6,7 +6,29 @@
  * @return {string[]}
  */
 var generateParenthesis = function(n) {
-    
+    // 这道题得用backtracking，
+    // 需要满足两个条件，第一个条件是当左括号数量多于或者等于n后就不能继续加了
+    // 第二个条件是 左括号的数量得多余右括号 因为不然的话相当于是no well-formed parentheses
+    let res = [];
+    let curr = []
+    let dfs = function(open, close) {
+        if (open === n && close === n) {
+            res.push(curr.join(""))
+            return;
+        }
+        if (open < n) {
+            curr.push("(");
+            dfs(open + 1, close);
+            curr.pop()
+        }
+        if (open > close) {
+            curr.push(")");
+            dfs(open, close + 1);
+            curr.pop();
+        }
+    }
+    dfs(0, 0);
+    return res;
 };
 
 let n = 3;
