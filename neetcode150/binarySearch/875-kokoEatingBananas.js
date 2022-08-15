fffd3c6515586c1d1ecc1ae0b046201a8f1120c4
Yinn <1499978024@qq.com>
2022-08-15 16:39:10 +0200

    875 koko eating bananas

--- neetcode150/binarySearch/875-kokoEatingBananas.js
+++ neetcode150/binarySearch/875-kokoEatingBananas.js
@@ -10,7 +10,35 @@
  * @return {number}
  */
 var minEatingSpeed = function(piles, h) {
-    
+    // 这道题的思路
+    // 这个koko吃东西的最多一小时是 max(...piles)
+    // 然后再从1 到 这个最大值找
+    // 用binary search
+    // h 一定在 1 到 max 之间
+    let maxValue = Math.max(...piles);
+    let l = 1, r = maxValue;
+    let res = r;
+    while (l <= r) {
+        // the speed of eating
+        let mid = Math.floor((l + r) / 2)
+        let time = 0; // the time to finish all the bananas
+        for (let pile of piles) {
+            time = time + Math.ceil(pile / mid);
+        }
+        if (time <= h) {
+            // it takes not many time to eat the banana
+            // so we need to decrease the mid;
+            // mid is too big
+            r = mid - 1;
+            res = Math.min(res, mid);
+        } else if (time > h) {
+            // out of time, mid too small
+            // it takes too many times to eat the banana
+            // we will increase the mid;
+            l = mid + 1;
+        }
+    }
+    return res;
 };
 
 let piles = [3,6,7,11], h = 8;
