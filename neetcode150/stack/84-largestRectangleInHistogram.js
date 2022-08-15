dbeecf4043c3e64f905055f7c9e2c140c29389b5
Yinn <1499978024@qq.com>
2022-08-15 15:59:31 +0200

    add 84 largest rectangle in histogram

---
+++ neetcode150/stack/84-largestRectangleInHistogram.js
@@ -0,0 +1,15 @@
+// 84. Largest Rectangle in Histogram
+// Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, return the area of the largest rectangle in the histogram.
+
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var largestRectangleArea = function(heights) {
+
+};
+
+let heights = [2,1,5,6,2,3];
+let heights1 = [2,4];
+console.log(largestRectangleArea(heights));
+console.log(largestRectangleArea(heights1));
