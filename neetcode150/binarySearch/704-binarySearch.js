a49177e32dc6a0705d9b61b9f1250d035065b165
Yinn <1499978024@qq.com>
2022-08-15 16:03:41 +0200

    704 binary search

--- neetcode150/binarySearch/704-binarySearch.js
+++ neetcode150/binarySearch/704-binarySearch.js
@@ -8,7 +8,18 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    
+    let left = 0, right = nums.length - 1;
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (nums[mid] > target) {
+            right = mid - 1;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            return mid;
+        }
+    }
+    return -1;
 };
 
 let nums = [-1,0,3,5,9,12], target = 9;
