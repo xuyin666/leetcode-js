97c7d96d726e8ded3312653910183840d03d4d7f
Yinn <1499978024@qq.com>
2022-08-15 14:58:23 +0200

    853 car fleet

--- neetcode150/stack/853-carFleet.js
+++ neetcode150/stack/853-carFleet.js
@@ -13,7 +13,35 @@
  * @return {number}
  */
 var carFleet = function(target, position, speed) {
-    
+    // 这道题的思路是先根据 position 拍个序
+    // 然后再比较到达终点的时间
+    // 比如说 如果第i辆车到达终点需要5个小时，前面的那辆车需要6个小时
+    // 则证明这两辆车一定会相遇 他们则某时刻可以合并成一辆车
+    // 倒序遍历那个position，如果t大于他前面车辆的t则push入stack
+    let posSpeed = [];
+    for (let i = 0; i < speed.length; i++) {
+        let tmp = [];
+        tmp.push(position[i]);
+        tmp.push(speed[i]);
+        posSpeed.push(tmp);
+    }
+    posSpeed.sort((a, b) => {
+        return a[0] - b[0];
+    })
+    let stack = [];
+    for (let i = posSpeed.length - 1; i >= 0; i--) {
+        let currCarTime = (target - posSpeed[i][0]) / posSpeed[i][1];
+        if (stack.length !== 0) {
+            let lastElem = stack[stack.length - 1];
+            let lastCarTime = (target - lastElem[0]) / lastElem[1];
+            if (lastCarTime < currCarTime) {
+                stack.push(posSpeed[i])
+            }
+        } else {
+            stack.push(posSpeed[i])
+        }
+    }
+    return stack.length;
 };
 
 let target = 12, position = [10,8,0,5,3], speed = [2,4,1,1,3];
