7bd81a43abca6e906dde04370aa734ee99a21b06
Yinn <1499978024@qq.com>
2022-08-15 13:48:30 +0200

    150 evaluate reverse polish notation

--- neetcode150/stack/150-evaluateReversePolishNotation.js
+++ neetcode150/stack/150-evaluateReversePolishNotation.js
@@ -9,8 +9,34 @@
  * @return {number}
  */
 var evalRPN = function(tokens) {
-    
+    let stack = [];
+    for (let token of tokens) {
+        if (token === "+") {
+            let right = stack.pop();
+            let left = stack.pop();
+            stack.push(left + right);
+        } else if (token === "-") {
+            let right = stack.pop();
+            let left = stack.pop();
+            stack.push(left - right);
+        } else if (token === "*") {
+            let right = stack.pop();
+            let left = stack.pop();
+            stack.push(left * right);
+        } else if (token === "/") {
+            let right = stack.pop();
+            let left = stack.pop();
+            stack.push(parseInt(left / right));
+        } else {
+            stack.push(parseInt(token));
+        }
+    }
+    return stack[0];
 };
+
+// 这道题思路是 遍历token然后检查那个对应的字符
+// 如果是数字则把值推入到stack中
+// 遇到 运算符号，再把值推出来，再把结果推入stack中
 
 let tokens = ["2","1","+","3","*"];
 let tokens1 = ["4","13","5","/","+"];
