313138700605b79f62d04d1a7f0e508f1cba0dba
Yinn <1499978024@qq.com>
2022-08-17 18:34:16 +0200

    115 distinct subsequences

--- neetcode150/2d-dynamocProgramming/115-distinctSubsequences.js
+++ neetcode150/2d-dynamocProgramming/115-distinctSubsequences.js
@@ -9,7 +9,26 @@
  * @return {number}
  */
 var numDistinct = function(s, t) {
-    
+    let dp = new Map();
+    let backtracking = function(i, j) {
+        if (j === t.length) return 1;
+        if (i === s.length) return 0;
+        let str = i + "," + j;
+        if (dp.has(str)) return dp.get(str);
+        // if s[i] != t[j] we need to see the next char of the s
+        if (s.charAt(i) !== t.charAt(j)) {
+            let res = backtracking(i + 1, j);
+            dp.set(str, res)
+            return res;
+        } else {
+            // backtracking(i + 1, j) means that we don't take the char i
+            // backtracking(i + 1, j + 1) means that we will take the char i 
+            let res = backtracking(i + 1, j) + backtracking(i + 1, j + 1)
+            dp.set(str, res)
+            return res;
+        }
+    }
+    return backtracking(0, 0);
 };
 
 let s = "rabbbit", t = "rabbit"
