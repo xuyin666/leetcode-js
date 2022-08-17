8f6ac311325cd5796aa329d60544a309081c5262
Yinn <1499978024@qq.com>
2022-08-17 18:26:56 +0200

    329 longest increasing path in a matrix

--- neetcode150/2d-dynamocProgramming/329-longestIncreasingPathInAMatrix.js
+++ neetcode150/2d-dynamocProgramming/329-longestIncreasingPathInAMatrix.js
@@ -8,7 +8,34 @@
  * @return {number}
  */
 var longestIncreasingPath = function(matrix) {
-    
+    let res = 0;
+    // we don't need path as a set, because we need to find
+    // an increasing path, else it can't go back to the previous value
+    // let path = new Set();
+    let dp = new Map();
+    let backtracking = function(row, col, preVal) {
+        if (row < 0 || row == matrix.length || 
+            col < 0 || col == matrix[0].length || 
+            matrix[row][col] <= preVal) 
+            return 0;
+        let str = row + "," + col;
+        // it should be wrote after the check of the condition
+        // because we need to check if matrix[row][col] <= preVal
+        if (dp.has(str)) return dp.get(str);
+        let a = backtracking(row + 1, col, matrix[row][col]);
+        let b = backtracking(row - 1, col, matrix[row][col]);
+        let c = backtracking(row, col - 1, matrix[row][col]);
+        let d = backtracking(row, col + 1, matrix[row][col]);
+        let maxVal = Math.max(a, b, c, d) + 1;
+        dp.set(str, maxVal);
+        return maxVal;
+    }
+    for (let row = 0; row < matrix.length; row++) {
+        for (let col = 0; col < matrix[0].length; col++) {
+            res = Math.max(res, backtracking(row, col, -1));
+        }
+    }
+    return res;
 };
 
 let matrix = [[9,9,4],[6,6,8],[2,1,1]];
