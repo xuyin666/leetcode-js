1fa895ccbfb950fc07d3b085da2862b2faa29caa
Yinn <1499978024@qq.com>
2022-08-17 14:10:26 +0200

    309 best time to buy aand sell stock with cooldown

--- neetcode150/2d-dynamocProgramming/309-bestTimeToBuyAndSellStockWithCooldown.js
+++ neetcode150/2d-dynamocProgramming/309-bestTimeToBuyAndSellStockWithCooldown.js
@@ -9,7 +9,38 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    
+    // i is the index that we are
+    // canBuy it means that in the index, if we can buy the new stock or not
+    let dp = new Map();
+    let backTracking = function(i, canBuy) {
+        let str = i + "," + canBuy;
+        if (dp.has(str)) return dp.get(str);
+        if (i >= prices.length) return 0;
+        // if we can buy now
+        if (canBuy) {
+            // if we buy now 
+            // we could not buy any stock;
+            let buyNow = backTracking(i + 1, false) - prices[i];
+            
+            // if we don't buy now 
+            let buyLater = backTracking(i + 1, true); 
+            let res = Math.max(buyNow, buyLater)
+            dp.set(str, res);
+            return res;
+        } else {
+            // if we can't buy the stock in the index i
+            // we can sell now or sell later
+            // if we sell now, for the next day, we can buy stock
+            // why i + 2, because you can't buy a stcok when you sell the stock
+            let sellNow = backTracking(i + 2, true) + prices[i];
+            // if we don't sell the stock now, we can't buy for the next day
+            let sellLater = backTracking(i + 1, false);
+            let res = Math.max(sellNow, sellLater);
+            dp.set(str, res);
+            return res;
+        }
+    }
+    return backTracking(0, true);
 };
 
 let prices = [1,2,3,0,2];
