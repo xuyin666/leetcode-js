a6a8709de9b534b2aea030920e000418d11d98be
Yinn <1499978024@qq.com>
2022-08-18 13:09:25 +0200

    287 find the duplicate number

--- neetcode150/linkedList/287-findTheDuplicateNumber.js
+++ neetcode150/linkedList/287-findTheDuplicateNumber.js
@@ -8,7 +8,23 @@
  * @return {number}
  */
 var findDuplicate = function(nums) {
-    
+    // 这道题的思路是用快慢指针，相遇了之后再放两个指针
+    // 一个指针在相交处 一个指针在最初的起点 
+    // 慢慢走 最后相遇的地方就是duplicate的值
+
+    let fast = nums[nums[0]];
+    let slow = nums[0];
+    while (fast !== slow) {
+        fast = nums[nums[fast]];
+        slow = nums[slow];
+    }
+    let start = nums[0];
+    let inter = nums[slow];
+    while (start !== inter) {
+        start = nums[start];
+        inter = nums[inter];
+    }
+    return start;
 };
 
 let nums = [1,3,4,2,2];
