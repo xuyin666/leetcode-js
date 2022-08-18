2f3b9b97b2a9059a88b58208177eb4e17502f459
Yinn <1499978024@qq.com>
2022-08-18 15:30:35 +0200

    add 74 serach a 2d matrix need to be modified

--- neetcode150/binarySearch/74-searchA2DMatrix.js
+++ neetcode150/binarySearch/74-searchA2DMatrix.js
@@ -9,7 +9,29 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-    
+    let rowLen = matrix.length;
+    let colLen = matrix[0].length;
+    let row = -1;
+    for (let i = 0; i < rowLen; i++) {
+        if (target >= matrix[i][0] && target <= matrix[i][colLen - 1]) {
+            row = i;
+            break;
+        }
+    }
+    if (row === -1) return false;
+
+    let left = 0, right = colLen - 1;
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2)
+        if (target < matrix[row][mid]) {
+            right = mid - 1;
+        } else if (target > matrix[row][mid]) {
+            left = mid + 1;
+        } else {
+            return true;
+        }
+    }
+    return false;
 };
 
 let matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3;
