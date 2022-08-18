6cf911a8a19967ee2b50353db53a98de84b6b7b4
Yinn <1499978024@qq.com>
2022-08-18 20:13:06 +0200

    130 surrounded regions

--- neetcode150/graphs/130-surroundedRegions.js
+++ neetcode150/graphs/130-surroundedRegions.js
@@ -7,7 +7,39 @@
  * @return {void} Do not return anything, modify board in-place instead.
  */
 var solve = function(board) {
-    
+    let visit = new Set();
+    let dfs = function(row, column) {
+        if (row < 0 || row >= board.length ||
+            column < 0 || column >= board[0].length ||
+            board[row][column] === "X")
+            return 0;
+        let str = row + "," + column;
+        if (visit.has(str)) return 0;
+        visit.add(str);
+        return dfs(row - 1, column) +
+                dfs(row + 1, column) +
+                dfs(row, column + 1) +
+                dfs(row, column - 1);
+    }
+    for (let i = 0; i < board.length; i++) {
+        // first column and last column
+        dfs(i, 0)
+        dfs(i, board[0].length - 1);
+    }
+    for (let i = 0; i < board[0].length; i++) {
+        // first row and last row
+        dfs(0, i);
+        dfs(board.length - 1, i);
+    }
+    console.log(visit);
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            let str = i + "," + j;
+            if (board[i][j] === "X" || visit.has(str))
+                continue;
+            board[i][j] = "X"
+        }
+    }
 };
 
 // Input: board = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
