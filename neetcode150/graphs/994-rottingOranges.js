f97442901a71c1f5167f5813a60f57e8e794c68d
Yinn <1499978024@qq.com>
2022-08-19 13:34:12 +0200

    994 rotting oranges

--- neetcode150/graphs/994-rottingOranges.js
+++ neetcode150/graphs/994-rottingOranges.js
@@ -11,7 +11,55 @@
  * @return {number}
  */
 var orangesRotting = function(grid) {
-    
+    // 用bfs mutlisource bfs
+    // 得检查有多少个新鲜橘子
+    // 如果没有新鲜橘子 就可以停了
+    let rowLen = grid.length;
+    let colLen = grid[0].length;
+    let fresh = 0;
+    let rotten = [];
+    for (let row = 0; row < rowLen; row++) {
+        for (let col = 0; col < colLen; col++) {
+            if (grid[row][col] === 1)
+                fresh = fresh + 1;
+            if (grid[row][col] === 2)
+                rotten.push([row, col])
+        }
+    }
+    let res = 0;
+    while (rotten.length > 0 && fresh > 0) {
+        let lenRotten = rotten.length;
+        for (let i = 0; i < lenRotten; i++) {
+            let rotOrange = rotten.shift();
+            let row = rotOrange[0];
+            let col = rotOrange[1];
+            if (row + 1 < rowLen && grid[row + 1][col] === 1) {
+                grid[row + 1][col] = 2;
+                rotten.push([row + 1, col]);
+                fresh = fresh - 1;
+            }
+            if (row - 1 >= 0 && grid[row - 1][col] === 1) {
+                grid[row - 1][col] = 2;
+                rotten.push([row - 1, col]);
+                fresh = fresh - 1;
+            }
+            if (col + 1 < colLen && grid[row][col + 1] === 1) {
+                grid[row][col + 1] = 2;
+                rotten.push([row, col + 1]);
+                fresh = fresh - 1;
+            }
+            if (col - 1 >= 0 && grid[row][col - 1] === 1) {
+                grid[row][col - 1] = 2;
+                rotten.push([row, col - 1]);
+                fresh = fresh - 1;
+            }
+        }
+        res = res + 1;
+    }
+    if (fresh === 0)
+        return res;
+    else 
+        return -1;
 };
 
 let grid = [[2,1,1],[1,1,0],[0,1,1]];
