d6806d371813788492bc2d1f34caba81ca35177a
Yinn <1499978024@qq.com>
2022-08-20 10:59:27 +0200

    684 redundant connection

--- neetcode150/graphs/684-redundantConnection.js
+++ neetcode150/graphs/684-redundantConnection.js
@@ -8,7 +8,43 @@
  * @return {number[]}
  */
 var findRedundantConnection = function(edges) {
-    
+    // 这道题的思路是 union and find（）
+    // 一旦发现 union返回的是false则 这证明已经被连接了
+    // 则代表需要返回当前的edge
+
+    let parent = new Array(edges.length + 1)
+    let rank = new Array(edges.length + 1).fill(1)
+    for (let i = 0; i <= edges.length; i++) {
+        parent[i] = i;
+    }
+
+    let find = function(node) {
+        let curr = node;
+        while (curr !== parent[curr]) {
+            parent[curr] = parent[parent[curr]];
+            curr = parent[curr];
+        }
+        return curr;
+    }
+
+    let union = function(node1, node2) {
+        let par1 = find(node1);
+        let par2 = find(node2);
+        if (par1 === par2) return false;
+        if (rank[par1] > rank[par2]) {
+            rank[par1] = rank[par1] + rank[par2];
+            parent[par2] = par1;
+        } else {
+            rank[par2] = rank[par2] + rank[par1];
+            parent[par1] = par2;
+        }
+        return true;
+    }
+
+    for (let edge of edges) {
+        if (!union(edge[0], edge[1]))
+            return edge;
+    }
 };
 
 let edges = [[1,2],[1,3],[2,3]];
