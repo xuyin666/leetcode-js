db14ad0bc84c388fb1ed3db6ae506b8482de4bd4
Yinn <1499978024@qq.com>
2022-08-24 14:55:48 +0200

    27 remove element

---
+++ specified-learning专项训练/slidingWindowAndTwoPointers/exercices/27-removeElement.py
@@ -0,0 +1,18 @@
+# 27. Remove Element
+# Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
+# Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+# Return k after placing the final result in the first k slots of nums.
+# Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+from typing import List
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        # 从0到j里面的元素都不为val [0, i)
+        i = 0
+        j = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[j] = nums[i]
+                j = j + 1
+        return j
+    
