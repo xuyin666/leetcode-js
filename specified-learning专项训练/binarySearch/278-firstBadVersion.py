0065f4bbe4aa61de833b0e86ce6f387a334792d8
Yinn <1499978024@qq.com>
2022-09-07 22:26:37 +0200

    278 first bad version

---
+++ specified-learning专项训练/binarySearch/278-firstBadVersion.py
@@ -0,0 +1,27 @@
+# 278. First Bad Version
+# You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+# Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+# You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        l = 1
+        r = n + 1
+        while l < r:
+            mid = l + (r - l) // 2
+            if isBadVersion(mid):
+                r = mid
+            else:
+                l = mid + 1
+        return l
+
+n = 5
+bad = 4
+print(Solution().firstBadVersion(n))
+n = 1
+bad = 1
+print(Solution().firstBadVersion(n))
