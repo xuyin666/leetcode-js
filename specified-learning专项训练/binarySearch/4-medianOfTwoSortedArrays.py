8a02e01124096fda0ed9edb0eb4a7ed78b7e9cf8
Yinn <1499978024@qq.com>
2022-09-10 19:14:32 +0200

    4 median of two sorted arrays

--- specified-learning专项训练/binarySearch/4-medianOfTwoSortedArrays.py
+++ specified-learning专项训练/binarySearch/4-medianOfTwoSortedArrays.py
@@ -5,7 +5,33 @@
 from typing import List
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        return 0
+        A = nums1
+        B = nums2
+        total = len(A) + len(B)
+        half = total // 2
+        if len(A) > len(B):
+            A, B = B, A
+        # A 的长度短于 B
+        l = 0
+        r = len(A) - 1
+        while True:
+            i = l + (r - l) // 2 # 取得A的中点
+            # 比方说  len(A) = 5 len(B) = 3
+            # i = 1 j 应该也等于 1
+            j = (half - 1) - i - 1 # B 的对应点
+            ALeft = A[i] if i >= 0 else float("-inf")
+            ARight = A[i+ 1] if i + 1 < len(A) else float("inf")
+            BLeft = B[j] if j >= 0 else float("-inf")
+            BRight = B[j + 1] if j + 1 < len(B) else float("inf")
+            if BLeft <= ARight and BRight >= ALeft:
+                if total % 2 == 1:
+                    return min(BRight, ARight)
+                else:
+                    return (max(ALeft, BLeft) + min(BRight, ARight)) / 2
+            elif BRight < ALeft:
+                r = i - 1
+            else:
+                l = i + 1
 
 nums1 = [1,3]
 nums2 = [2]
@@ -13,3 +39,7 @@ print(Solution().findMedianSortedArrays(nums1, nums2))
 nums1 = [1,2]
 nums2 = [3,4]
 print(Solution().findMedianSortedArrays(nums1, nums2))
+nums1 = []
+nums2 = [1]
+print(Solution().findMedianSortedArrays(nums1, nums2))
+
